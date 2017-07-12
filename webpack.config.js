fd6533ba480260407be44211d806854d72b03aa8
Jasonzj <925645402@qq.com>
2017-07-12 19:29:13 +0800

    modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
 
-moudle.exports = {
-    entry: './src/entry.js',
+module.exports = {
+    entry: ['./src/entry.js'],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'js/[name].bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.scss', '.css'],
-        moudle: [
+        modules: [
             path.resolve(__dirname, 'node_modules'),
             path.join(__dirname, './src')
         ],
@@ -22,11 +23,11 @@ moudle.exports = {
             "utils" : path.resolve(__dirname, 'src/utils')
         }
     },
-    moudle: {
+    module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: path.resolve(__dirname, 'node_modules'),
                 query: {
                     presets: ['es2015', 'react']
@@ -34,12 +35,14 @@ moudle.exports = {
             },
             {
                 test: /\.(scss|css)$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
                 use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'sass-loader', 'postcss-loader']
+                    use: ["css-loader", "sass-loader", "postcss-loader"]
                 })
             },
             {
                 test: /\.(png|jpg|gif|svg)$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
                 use: ['url-loader?limit=1000&name=files/[md5:hash:base64:10].[ext]']
             }
         ]
@@ -50,13 +53,17 @@ moudle.exports = {
         historyApiFallback: true,
         port: 8000,
         contentBase: path.resolve(__dirname, "build"),
+        stats: {
+            modules: false,
+            chunks: false
+        }
     },
-    plugin: [
+    plugins: [
         new ExtractTextPlugin('styles.css'),
         new htmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html'
-        })
-    ],
-    postcss: [autoprefixer()]
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }

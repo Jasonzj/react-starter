03b458e9352b56c183e8ed8165e52f91ea44676e
Jasonzj <925645402@qq.com>
2017-09-30 18:55:53 +0800

    add proxy

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+const proxy = require('http-proxy-middleware')
+
+const port = 3000
+const host = 'localhost'
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log('Listening at http://localhost:3000/');
-});
+    publicPath: config.output.publicPath,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    stats: {
+        modules: false,
+        chunks: false
+    },
+    setup(app) {
+        app.use('/book/*', proxy({
+            target: 'https://www.easy-mock.com/mock/593611b991470c0ac101d474',
+            secure: false
+        }))
+    }
+}).listen(port, host, (err, result) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Listening at http://localhost:3000/')
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Bundle from './bundle.js'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import asyncComponent from './AsyncComponent'
 import {
     HashRouter as Router,
     Route,
@@ -17,21 +17,14 @@ import HelloWorld from 'containers/HelloWorld'
 import NotFound from 'containers/404.jsx'
 
 // lazyContainer
-import HelloReact from 'bundle-loader?lazy&name=HelloReact-[name]!containers/HelloReact/index.js'
+const HelloReact = asyncComponent(() => import(/* webpackChunkName: "HelloReact" */ './containers/HelloReact/index'))
 
 // style
 import './app.scss'
 
-// router
-const createComponent = component => () => (
-    <Bundle load={component}>
-        { Component => <Component /> }
-    </Bundle>
-)
-
 // store
 let store = null
-if (__DEV__) {  // 开发环境
+if (__DEV__) { // 开发环境
     store = createStore(
         counter,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -46,7 +39,7 @@ const App = () => (
             <HomeContainer>
                 <Switch>
                     <Route exact path="/" component={HelloWorld} />
-                    <Route path="/react" component={createComponent(HelloReact)} />
+                    <Route path="/react" component={HelloReact} />
                     <Route component={NotFound} />
                 </Switch>
             </HomeContainer>
